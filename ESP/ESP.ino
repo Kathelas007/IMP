c0a1b693dc80c809aabd91130121db44785701cd
AwesomeNicknameEver <katerina.muskova@gmail.com>
2020-12-19 17:47:08 +0100

    asi done

--- ESP/ESP.ino
+++ ESP/ESP.ino
@@ -26,6 +26,7 @@ typedef struct Led_struct {
 Led_struct leds = { .nums = {4, 0, 2}, .state = {false, false, false}, .on_s = {HIGH, HIGH, LOW}, .off_s = {LOW, LOW, HIGH}};
 
 bool is_sequence_running = false;
+int selected_sequence;
 
 // *** SERVER ***
 
@@ -35,6 +36,8 @@ const char *ssid = "ESPAccesPoint";
 const char *password = "esp123456789";
 
 // *********** FUNCTIONS ************************************************
+
+// *********** server related functions
 
 void handleRoot(AsyncWebServerRequest * request) {
   Serial.println("handling root");
@@ -46,11 +49,11 @@ void handleLeds(AsyncWebServerRequest * request) {
 
   int ledId = request->getParam(0)->value().toInt();
   int state = request->getParam(1)->value().toInt();
-  
+
   Serial.print("handling led "); Serial.print(ledId);
   Serial.print(" state "); Serial.println(state);
 
-  leds.state[ledId] = state; 
+  leds.state[ledId] = state;
 
   AsyncResponseStream *response = request->beginResponseStream("application/json");
   DynamicJsonBuffer jsonBuffer;
@@ -61,11 +64,65 @@ void handleLeds(AsyncWebServerRequest * request) {
   request->send(response);
 }
 
+AsyncWebServerRequest * request_to_seq;
+
+void sendSequenceResponse() {
+  is_sequence_running = false;
+  request_to_seq->send(200);
+}
+
 void handleSequences(AsyncWebServerRequest * request) {
   if (request->params() != 1) return;
+  if (is_sequence_running) return;
 
   int seqId = request->getParam(0)->value().toInt();
-  Serial.println("handling seq " + request->getParam(0)->value());
+  Serial.print("handling seq "); Serial.println(request->getParam(0)->value());
+
+  request_to_seq = request;
+  selected_sequence = seqId;
+  is_sequence_running = true;
+}
+
+// *********** arduino related functions
+void run_seq(int seq) {
+  for (int i = 0; i < N; i++) {
+    digitalWrite(leds.nums[i], leds.off_s[i]);
+    delay(100);
+  }
+
+  switch (seq) {
+    case 0:
+      for (int i = 0; i < N; i++) {
+        for (int j = 0; j < 3; j++) {
+          digitalWrite(leds.nums[i], leds.on_s[i]);
+          delay(200);
+          digitalWrite(leds.nums[i], leds.off_s[i]);
+          delay(100);
+        }
+      }
+      break;
+    case 1:
+      digitalWrite(leds.nums[0], leds.on_s[0]);
+      digitalWrite(leds.nums[1], leds.on_s[1]);
+
+      for (int i = 0; i < 20; i++) {
+        digitalWrite(leds.nums[i % 3], leds.off_s[i]);
+        digitalWrite(leds.nums[(i + 2) % 3], leds.on_s[i]);
+        delay(200);
+      }
+      break;
+    case 2:
+      for (int i = 0; i < 30; i++) {
+        int id = random(3);
+        digitalWrite(leds.nums[id], leds.off_s[id]);
+        id = random(3);
+        digitalWrite(leds.nums[id], leds.on_s[id]);
+        delay(50);
+      }
+      break;
+    default:
+      break;
+  }
 }
 
 // *************** SETUP *************************************************
@@ -81,7 +138,7 @@ void setup() {
   delay(1000);
   Serial.begin(115200);
 
-  // start SPIFFS 
+  // start SPIFFS
   Serial.print("\n\nSetting SPIFFS ... ");
   while (!SPIFFS.begin()) {
     delay(500);
@@ -103,19 +160,19 @@ void setup() {
   server.on("/", HTTP_GET, handleRoot);
   server.on("/led", HTTP_GET, handleLeds);
   server.on("/seq", HTTP_GET, handleSequences);
-  
+
 
   // set bootstrap routes
-  server.on("/src/bootstrap.bundle.min.js", HTTP_GET, [](AsyncWebServerRequest *request){
-      request->send(SPIFFS, "/src/bootstrap.bundle.min.js", "text/javascript");
+  server.on("/src/bootstrap.bundle.min.js", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/src/bootstrap.bundle.min.js", "text/javascript");
   });
-   
-  server.on("/src/jquery-3.3.1.min.js", HTTP_GET, [](AsyncWebServerRequest *request){
-      request->send(SPIFFS, "/src/jquery-3.3.1.min.js", "text/javascript");
+
+  server.on("/src/jquery-3.3.1.min.js", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/src/jquery-3.3.1.min.js", "text/javascript");
   });
-   
-  server.on("/src/bootstrap.min.css", HTTP_GET, [](AsyncWebServerRequest *request){
-      request->send(SPIFFS, "/src/bootstrap.min.css", "text/css");
+
+  server.on("/src/bootstrap.min.css", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/src/bootstrap.min.css", "text/css");
   });
 
   server.begin();
@@ -126,11 +183,17 @@ void setup() {
 // *************** LOOP *************************************************
 void loop() {
 
-  for(int i=0; i<N; i++){
-      if (leds.state[i])
-        digitalWrite(leds.nums[i], leds.on_s[i]);
-      else
-        digitalWrite(leds.nums[i], leds.off_s[i]);
-    }
+  if (is_sequence_running) {
+    run_seq(selected_sequence);
+    sendSequenceResponse();
+    Serial.println("sequence is done");
+  }
+
+  for (int i = 0; i < N; i++) {
+    if (leds.state[i])
+      digitalWrite(leds.nums[i], leds.on_s[i]);
+    else
+      digitalWrite(leds.nums[i], leds.off_s[i]);
+  }
 
 }
