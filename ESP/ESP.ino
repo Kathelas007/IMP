f01a4ff4414155fb3dceb914015cdfd8e87cc2d6
AwesomeNicknameEver <katerina.muskova@gmail.com>
2020-12-19 14:54:57 +0100

    leds

--- ESP/ESP.ino
+++ ESP/ESP.ino
@@ -4,10 +4,14 @@
 #include <ESPAsyncWebServer.h>
 #include <ESPAsyncTCP.h>
 
+#include <AsyncJson.h>
+#include <ArduinoJson.h>
+
 #include <FS.h>
 #include <Wire.h>
 
 // ************ GLOBAL ****************************************************
+
 // *** LEDS ***
 
 const int N = 3;
@@ -21,6 +25,8 @@ typedef struct Led_struct {
 
 Led_struct leds = { .nums = {4, 0, 2}, .state = {false, false, false}, .on_s = {HIGH, HIGH, LOW}, .off_s = {LOW, LOW, HIGH}};
 
+bool is_sequence_running = false;
+
 // *** SERVER ***
 
 AsyncWebServer server(80);
@@ -32,13 +38,37 @@ const char *password = "esp123456789";
 
 void handleRoot(AsyncWebServerRequest * request) {
   Serial.println("handling root");
-  request->send(SPIFFS, "/data/index.html");
+  request->send(SPIFFS, "/index.html");
 }
 
-void handleLeds() {
+void handleLeds(AsyncWebServerRequest * request) {
+  if (request->params() != 2) return;
+
+  int ledId = request->getParam(0)->value().toInt();
+  int state = request->getParam(1)->value().toInt();
+  
+  Serial.print("handling led "); Serial.print(ledId);
+  Serial.print(" state "); Serial.println(state);
+
+  leds.state[ledId] = state; 
+
+  request->send(200, "application/json", "{\"message\":\"Welcome\"}");
+  
+//
+//  AsyncResponseStream *response = request->beginResponseStream("application/json");
+//  DynamicJsonBuffer jsonBuffer;
+//  JsonObject &root = jsonBuffer.createObject();
+//  root["led"] = ledId;
+//  root["state"] = state;
+//  root.printTo(*response);
+//  request->send(response);
 }
 
-void handleSequences() {
+void handleSequences(AsyncWebServerRequest * request) {
+  if (request->params() != 1) return;
+
+  int seqId = request->getParam(0)->value().toInt();
+  Serial.println("handling seq " + request->getParam(0)->value());
 }
 
 // *************** SETUP *************************************************
@@ -54,11 +84,12 @@ void setup() {
   delay(1000);
   Serial.begin(115200);
 
-  // start SPIFFS (saved inde.xml)
+  // start SPIFFS 
+  Serial.print("\n\nSetting SPIFFS ... ");
   while (!SPIFFS.begin()) {
     delay(500);
   };
-  Serial.print("SPIFFS is ready");
+  Serial.println("OK");
 
   // set acces point
   Serial.print("Setting access point... ");
@@ -71,17 +102,13 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(APIP);
 
-  //start server
-  //  server.on("/", HTTP_GET, handleRoot());
-  //  server.on("/led", HTTP_GET, handleLeds());
-  //  server.on("/seq", HTTP_GET, handleSequences());
-  //  server.begin();
+  // set web routes
+  server.on("/", HTTP_GET, handleRoot);
+  server.on("/led", HTTP_GET, handleLeds);
+  server.on("/seq", HTTP_GET, handleSequences);
+  
 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
-    Serial.println("handling root");
-    request->send(SPIFFS, "/index.html");
-  });
-
+  // set bootstrap routes
   server.on("/src/bootstrap.bundle.min.js", HTTP_GET, [](AsyncWebServerRequest *request){
       request->send(SPIFFS, "/src/bootstrap.bundle.min.js", "text/javascript");
   });
@@ -94,19 +121,19 @@ void setup() {
       request->send(SPIFFS, "/src/bootstrap.min.css", "text/css");
   });
 
-    server.on("/plain", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send(200, "text/plain", "Hello World");
-  });
-
-  
   server.begin();
-
-  Serial.println("HTTP server is running\n");
-
+  Serial.println("HTTP server is running\n\n\n");
 }
 
 
 // *************** LOOP *************************************************
 void loop() {
+
+  for(int i=0; i<N; i++){
+      if (leds.state[i])
+        digitalWrite(leds.nums[i], leds.on_s[i]);
+      else
+        digitalWrite(leds.nums[i], leds.off_s[i]);
+    }
 
 }
